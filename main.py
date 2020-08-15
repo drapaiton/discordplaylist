ffdbea8df62b7e4582345b0d38c361b5e5d6bbaa
ticua07 <58951699+ticua07@users.noreply.github.com>
2020-08-15 12:08:53 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,126 +1,49 @@
-import uinput
-from io import open
+import keyboard as kb
+import json
 import time
-import re
+default_settings = """{
+    "Speed": "0.05",
+    "Prefix": "-p" 
+}"""
+print(default_settings)
 
-#TODO: add uppper compatibility
 
-file_with_songs="songs.txt"
-file_with_settings="settings.txt"
-default_settings = """\
-Speed= 0.05
-Prefix= -p"""
+fh = open('songs.txt')
 
-def writeKeyboard(to_write:str,writing_speed:float):
-    events = (
-        uinput.KEY_A,
-        uinput.KEY_B,
-        uinput.KEY_C,
-        uinput.KEY_D,
-        uinput.KEY_E,
-        uinput.KEY_F,
-        uinput.KEY_G,
-        uinput.KEY_H,
-        uinput.KEY_I,
-        uinput.KEY_J,
-        uinput.KEY_K,
-        uinput.KEY_L,
-        uinput.KEY_M,
-        uinput.KEY_N,
-        uinput.KEY_O,
-        uinput.KEY_P,
-        uinput.KEY_Q,
-        uinput.KEY_R,
-        uinput.KEY_S,
-        uinput.KEY_T,
-        uinput.KEY_U,
-        uinput.KEY_V,
-        uinput.KEY_W,
-        uinput.KEY_X,
-        uinput.KEY_Y,
-        uinput.KEY_Z,
-        uinput.KEY_ENTER,
-        uinput.KEY_SPACE,
-        uinput.KEY_KPMINUS,
-        uinput.KEY_KPEQUAL
-        )
+songs = [line.rstrip() for line in fh.readlines()]
+
+fh.close()
+
+try:
+    f = open("settings.json", "r")
+except FileNotFoundError:
+    print("Settings.json no existe!")
+    exit()
+settings = json.load(f)
+
+if float(settings["Speed"]) < 0.05 or float(settings["Speed"]) > 0.5:
+    print("Configuracion invalida, usando parametros por defecto")
+    print("Esperando 5 secs para que entres al discord")
+    time.sleep(7) # pongo 7 por si acaso :D
+    for i in songs:
+        kb.write(settings["Prefix"])
+        kb.press("space")
+        for a in i:
+            time.sleep(0.05)
+            kb.press(a)
+        kb.press("enter")
     
-    with uinput.Device(events) as device:
-        translations = {" ":"space","\\":"enter","-":"kpminus","=":"kpequal"}
-        for ch in to_write:
-            time.sleep(writing_speed)
-            if ch in translations.keys():
-                ch = translations[ch]
-                time.sleep(writing_speed*10)
-            
-            eval(f"device.emit_click(uinput.KEY_{ch.upper()})")
+    exit(0)
 
-def filterEverySong(playlist:str):
-    only_Alphabet = re.sub("[^a-zA-Z\n\s]+","", playlist)
-    return only_Alphabet
 
-def addThePrefix(filtered_songs:str,prefix:str):
-    prefix = prefix.upper()
-    output = re.sub("[\n]+",f"\n{prefix} ",filtered_songs)
-    return str(prefix+" "+output)
 
-def separateInLines(prefixed_songs:str):
-    #Causes more process, but gain performance
-    #decreasing use of writekeyboard
-    final = prefixed_songs.split("\n")
-    def __removeFinalSpace(iterable:list):
-        return [i[:-1] if i[-1]==" " else i for i in iterable]
-    return __removeFinalSpace(final)
-
-def createNewFile(namefile:str,content:str=""):
-    try:
-        with open(namefile,"w",encoding="utf8") as f:
-            f.seek(0)
-            f.write(content)
-            print("the file was created, you can edit the .txt file")
-    except Exception as i:
-        print(i)
-        print("\ni cant create the file, do i have permissions ?")
-
-def __openfile(namefile:str):
-    with open(namefile,"r+",encoding="utf8") as f:
-        return f.read()
-
-def __firstConfigurations():
-    files = {file_with_settings:str,file_with_songs:str}
-    for k in files.keys():
-        content = openOrMakeIt(k)
-        files[k] = content
-    return files
-
-def openOrMakeIt(namefile:str):
-    try:
-        content = __openfile(namefile)
-    except FileNotFoundError as e:
-        print(f"i cant find {namefile} file")
-        if namefile == file_with_settings:
-            content = default_settings
-        else:
-            content = ""
-        createNewFile(namefile,content)
-    return content
-    
-def main():
-    files = __firstConfigurations()
-    
-    settings = files[file_with_settings].split("\n")
-    
-    writing_speed = float(settings[0].replace("Speed= ",""))
-    prefix        = settings[1].replace("Prefix= ","")
-    
-    song_list  = files[file_with_songs]
-    validsongs = filterEverySong(song_list)
-    full_text  = addThePrefix(validsongs,prefix)
-    
-    separated_commands = separateInLines(full_text)
-    for i in separated_commands:
-        writeKeyboard(i,writing_speed)
-        writeKeyboard("\\",writing_speed)
-        
-if __name__ == "__main__":
-    main()
+time_per_key = float(settings["Speed"])
+print("Esperando 5 secs para que entres al discord")
+time.sleep(7) # pongo 7 por si acaso :D
+for i in songs:
+    kb.write(settings["Prefix"])
+    kb.press("space")
+    for a in i:
+        time.sleep(time_per_key)
+        kb.press(a)
+    kb.press("enter")
